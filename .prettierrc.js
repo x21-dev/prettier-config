be7ad19964d67bcbc625cc48aec125282c86baf8
Simon Ho <superkhau@gmail.com>
2024-07-19 11:42:38 +0900

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+export default {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: false,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: true,
+  printWidth: 80,
+  // quoteProps: leave to eslint as prettier doesn't support always
+  semi: false,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
